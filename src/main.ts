20d74f46cb7f3e766621fdf4316ff8171a87c590
AlvaroGaertnerGit <alvarogaertnerufv18@gmail.com>
2025-02-06 16:55:14 +0100

    configuración rutas

--- src/main.ts
+++ src/main.ts
@@ -4,12 +4,19 @@ import { provideHttpClient } from '@angular/common/http';
 import { provideRouter, Route } from '@angular/router';
 import { VisualizationComponent } from './app/components/visualization/visualization.component';
 import { VisualizationZetaComponent } from './app/components/visualization-zeta/visualization-zeta.component';
+import { VisualizationMandelbrotComponent } from './app/components/visualization-mandelbrot/visualization-mandelbrot.component';
+import { LocationStrategy, PathLocationStrategy } from '@angular/common'; // Importar LocationStrategy
 
 const routes: Route[] = [
   { path: '', component: VisualizationComponent },
   { path: 'visualizationzeta', component: VisualizationZetaComponent },
+  { path: 'visualizationMandelbrot', component: VisualizationMandelbrotComponent },
 ];
 
 bootstrapApplication(AppComponent, {
-  providers: [provideRouter(routes), provideHttpClient()],
+  providers: [
+    provideRouter(routes),  // Rutas
+    provideHttpClient(),    // Cliente HTTP
+    { provide: LocationStrategy, useClass: PathLocationStrategy }, // Habilitar HTML5 routing
+  ]
 });
